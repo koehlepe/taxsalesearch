ff2cfc19b84e8391fbe2b38ac8198149d4a923a1
Peter Koehler <koehlepe@umich.edu>
2021-04-17 12:40:30 -0400

    Move from generic namespace to site specific

---
+++ SearchTaxSales/SearchTaxSales/Models/Property.cs
@@ -0,0 +1,23 @@
+    
+using System.ComponentModel.DataAnnotations;
+using SolrNet.Attributes;
+
+namespace SearchTaxSales.Models
+{
+    public class Property
+    {
+        [SolrUniqueKey("id")]
+        public string Id { get; set; }
+
+        [SolrField("title")]
+        public string[] Title { get; set; }
+
+        [SolrField("comments")]
+        public string[] Comments { get; set; }
+
+        [SolrField("link")]
+        public string[] Link { get; set; }
+
+    }
+
+}
